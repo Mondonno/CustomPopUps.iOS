9e8817fe04601d222a87d209ba1da9fd96071754
ezsource <example@gmail.com>
2020-09-05 18:01:31 +0200

    Added PopUpsDemoCode and update some things

---
+++ Examples/Projects/CustomPopUpDemo/CPopupLibrary/PopUpsObjects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CPopupLibrary
+{
+    public class PopUpsObjects : CustomPopUpViewController
+    {
+        //Working on it... (Alpha)
+        
+        public enum AniamtionType
+        {
+            Normal,
+            Slide,
+            Bounce
+        }
+        public enum Animated
+        {
+            Yes,
+            No,
+        }
+    }
+}
